dbb8a69943da67b2c5908a727cc36c647132f7ea
haipq07 <haipq07@gmail.com>
2024-12-04 17:07:11 -0800

    set up profile_manager and user_profile

---
+++ profile_manager.py
@@ -0,0 +1,29 @@
+class ProfileManager:
+    def __init__(self):
+        pass
+    def add_profile(self,name, location, relationshiop_status, age, occupation, astrological_sign, status=""):
+        pass
+    
+    def get_profile(self,name):
+        pass
+
+    def remove_profile(self,name):
+        pass
+
+    def connect_profile(self, name1, name2, weight=0):
+        pass
+    
+    def display_profiles(self):
+        pass
+    
+    def display_profile_details(self, name):
+        pass
+    
+    def get_friends_of_friends(self, name):
+        pass
+    
+    def read_profiles_from_csv(self, file_path):
+        pass
+    
+    def create_user_graph(self, current_user,depth = 1):
+        pass
