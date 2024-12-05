0dc775ead4ab1f5fd9384e3e43659b2f4d780688
haipq07 <haipq07@gmail.com>
2024-12-04 16:50:55 -0800

    set up file graph_adt.py

---
+++ graph_adt.py
@@ -0,0 +1,47 @@
+class Vertex:
+    def __init__(self,key):
+        self.key=key
+    def add_neighbor(self, nbr, weight = 0):
+        pass
+    def get_connection(self):
+        pass
+    def get_id(self):
+        pass
+    def get_weight(self, nbr):
+        pass
+
+class Undirected_graph:
+    def __init__(self):
+        pass
+    def add_vertex(self,key):
+        self.key=key
+    def get_vertex(self,key):
+        pass
+    
+    def add_edge(self, from_keu, to_key, weight=0):
+        pass
+
+    def get_vertices(self):
+        pass
+
+    def contains(self,key):
+        pass
+
+    def clear (self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def get_edges(self):
+        pass
+
+    def bfs(self,start):
+        pass
+
+    def dfs(self,start):
+        pass
+    
